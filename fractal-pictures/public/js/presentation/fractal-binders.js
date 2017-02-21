80f4915cf936017afb6404f157daf2b139a71ea6
Carles Capellas <carlesc@byrom.plc.uk>
2017-02-21 18:00:06 +0100

    Fractal-pictures: Finalize first canvas version implementation

--- fractal-pictures/public/js/presentation/fractal-binders.js
+++ fractal-pictures/public/js/presentation/fractal-binders.js
@@ -12,6 +12,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		fractalDrawer: document.getElementById('fractal-drawer'),
 		loader: document.getElementById('loader')
 	};
+	htmlNodes.canvasContext = htmlNodes.fractalPicture.getContext('2d');
+	var resizeCanvasTimeout;
 
 	var prompt = {
 		body: '<p>In the near future you will be able to modify the fill color in this modal. The behaviour is not yet implemented</p>',
@@ -35,42 +37,37 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		}
 	};
 
+	function fillCanvas(fractalResult) {
+		var piecePixelSize = htmlNodes.fractalPicture.width / fractal.resultColumns;
+  		htmlNodes.canvasContext.fillStyle = 'skyblue';
+
+		for (var i = 0; i < fractalResult.length; ++i) {			
+			for (var j= 0; j < fractalResult[i].length; ++j) {
+				if (fractalResult[i][j]) {
+					htmlNodes.canvasContext.fillRect(j * piecePixelSize, i * piecePixelSize, piecePixelSize, piecePixelSize);
+				}
+			}
+		}
+
+  		setTimeout(() => {
+			htmlNodes.fractalPicture.style.display = 'block';
+  			htmlNodes.loader.style.display = 'none';
+  		}, 1000);
+  	}
+
 	function fractalDrawerHandler() {			
       	htmlNodes.fractalControls.classList.remove('show');
       	htmlNodes.loader.style.display = 'block';
-		htmlNodes.fractalPicture.innerHTML = '';
+		htmlNodes.canvasContext.fillStyle = 'white';
+  		htmlNodes.canvasContext.clearRect(0, 0, htmlNodes.fractalPicture.width, htmlNodes.fractalPicture.height);
+  		htmlNodes.fractalPicture.style.display = 'none';
 		
 		fractal.zoomOut = parseInt(htmlNodes.zoomOut.value);
 		fractal.resultRows = Math.pow(fractal.patternRows, fractal.zoomOut);
 		fractal.resultColumns = Math.pow(fractal.patternColumns, fractal.zoomOut);
 
-		var fractalWidth = htmlNodes.fractalPicture.clientWidth;
-		var piecePixelSize = fractalWidth / fractal.resultColumns;
-		console.log(piecePixelSize);
-		var piecePercentageSize = Math.floor(piecePixelSize * 100 * 100 / fractalWidth) / 100;
-
       	return fractalService.computeFractal(fractal, sectionValueRetriever)
-      	.then((fractalResult) => {
-      		var canvasContext = document.getElementById('canvas-fractal-picture').getContext('2d');
-  			canvasContext.fillStyle = 'white';
-      		canvasContext.clearRect(0, 0, document.getElementById('canvas-fractal-picture').width, document.getElementById('canvas-fractal-picture').height);
-  			canvasContext.fillStyle = 'skyblue';
-      		var fractalHtml = '';
-			for (var i = 0; i < fractalResult.length; ++i) {			
-				for (var j= 0; j < fractalResult[i].length; ++j) {
-					fractalHtml += '<span class="piece ' + (fractalResult[i][j] ? 'colorful' : '') + '" style="width: ' + piecePercentageSize
-					 + '%; height:' + piecePercentageSize + '%;"></span>';
-					if (fractalResult[i][j]) {
-						canvasContext.fillRect(j * piecePixelSize, i * piecePixelSize, piecePixelSize, piecePixelSize);
-					}
-				}
-				fractalHtml += '<br />';
-			}
-      		setTimeout(() => {
-      			htmlNodes.fractalPicture.innerHTML = fractalHtml;
-      			htmlNodes.loader.style.display = 'none';
-      		}, 1000);
-      	});		
+      	.then(fillCanvas);
 	}
 
 	function gridDrawerHandler() {
@@ -102,6 +99,20 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		target.classList.toggle('checked');
 	}
 
+	function resizeCanvas() {
+		if(resizeCanvasTimeout) {
+			clearTimeout(resizeCanvasTimeout);
+		}
+		resizeCanvasTimeout = setTimeout(function() {
+			var canvasSize = Math.min(window.innerWidth, window.innerHeight);
+			htmlNodes.fractalPicture.width = canvasSize;
+			htmlNodes.fractalPicture.height = canvasSize;
+			if(fractal.result && fractal.result.length > 0) {
+				fillCanvas(fractal.result);
+			}
+		}, 400);
+	}
+
 	function sectionValueRetriever (row, column) {
 		var currentSection = document.querySelector('#pattern span[data-row="' + row + '"][data-column="' + column + '"]');
 		var sectionClasses = Array.from(currentSection.classList);
@@ -111,5 +122,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	htmlNodes.rowsNumber.addEventListener('keyup', gridDrawerHandler);
 	htmlNodes.columnsNumber.addEventListener('keyup', gridDrawerHandler);
 	htmlNodes.fractalDrawer.addEventListener('click', fractalDrawerHandler);
+	window.addEventListener('resize', resizeCanvas);
+
 	gridDrawerHandler();
+	resizeCanvas();
 });
