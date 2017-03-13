529cdeb6708b16383079376e4ee7a8998c6ea94b
Carles Capellas <carlesc@byrom.plc.uk>
2017-03-13 13:54:32 +0100

    Pull-joke: Create jokes-services and implement the getTodayJoke function

--- apps/pull-joke/index.js
+++ apps/pull-joke/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var jokesService = require('./services/joke-service');
 
 router.get('/', function (req, res, next) {
+	var todayJoke = jokesService.getTodayJoke();
 	res.render('pull-joke-index', {
-		joke: 'Working on it...'
+		joke: todayJoke
 	});
 });
 
---
+++ apps/pull-joke/services/joke-service.js
@@ -0,0 +1,194 @@
+var jokes = [
+
+	'Como maldice un pollo a otro pollo? Caldito seas!',
+
+	'- Cariño, pronto sera nuestro aniversario. Que me vas a regalar?<br />' +
+	'- Ves ese coche aparcado? Pues una liquadora del mismo color',
+
+	'La noche de bodas, el novio encuentra a la novia en la cama con un enano.<br />' + 
+	'- ¿Y ese quién es?<br />' +
+	'- El aperitivo!',
+
+	'Un hombre llega a casa y se encuentra a su mujer en la cama con otro.<br />' +
+	'- ¿Y ese quién es?<br />' +
+	'- Ése es quien paga el piso, la luz, la comida, el colegio de las niñas, ...<br />' +
+	'- Tápalo, tápalo! Que no se resfrie!',
+
+	'- Oye, ¿tu mujer a que se dedica?<br />' +
+	'- Mi mujer es jinete de aves de corral<br />' +
+	'- Hostia, ¿Y eso que es?<br />' +
+	'- Que cada dos por tres me monta un pollo',
+	
+	'- ¿Tienes hijos?<br />' +
+	'- Sí, una niña. Está en esa edad que se lo mete todo en la boca.<br />' +
+	'- ¿6 meses?<br />' +
+	'- ¡Qué va! 16 años.',
+
+	'- ¿Cómo se declara el acusado?<br />' +
+	'- Con flores. Soy bastante tradicional.',
+
+	'- ¡Paco quita el futbol y baja la basura!<br />' +
+	'- ¡Cualquier día de estos cojo la puerta y...!<br />' +
+	'- ¡¿Y!?<br />' +
+	'- Y la lijo y la barnizo, que falta le hace...',
+
+	'Solo cuando se posa un mosquito en tus testículos te das cuenta que no todo se soluciona con violencia',
+
+	'- ¿Lobezno?<br />' +
+	'- ¿Que zi veo el qué?<br />' +
+	'- ¡CORTEN!... ¡Por decimotercera vez, deja de hacer la broma ya y cíñete al guión joder!',
+
+	'- Que fea es esa tía<br />' +
+	'- Es mi madre<br />' +
+	'- No, digo la de al lado<br />' +
+	'- Esa es mi hermana<br />' +
+	'- Pues pégame dos hostias q esto ya no hay quien lo arregle',
+
+	'- Nunca me escuchas.<br />' +
+	'- Y yo a ti.',
+
+	'- Cari, di a mis padres lo que me dijiste anoche<br />' +
+	'- Ehhh... ¿Que me pones la polla como el pescuezo de un cantaor?<br />' +
+	'- No, lo de la boda, gilipollas',
+
+	'- En boca es plano, le falta pasión, textura, apenas notó el bacalao...<br />' +
+	'- Manolo, cómeme el coño y deja de jugar a master chef',
+
+	'- ¿Y tu desde cuando llevas pendiente, Paco?<br />' +
+	'- Desde que mi mujer se lo encontró en el coche y le dije que era mío...',
+
+	'- ¿Quieres que te mire a los ojos cuando me lo meta en la boca?<br />' +
+	'- Señorita, compórtese y sople el alcoholímetro por favor.',
+
+	'- Hija, deja de ligar con el panadero, podría ser tu padre.<br />' +
+	'- Me da igual que sea mayor que yo.<br />' +
+	'- Creo que no me has entendido.',
+
+	'- Cariño, hoy que vamos con el tiempo justo, lo mejor va a ser una comida rápida.<br />' +
+	'- ¿Qué te apetece, Paco? ¿Pizza?<br />' +
+	'- Creo que no me has entendido.',
+
+	'- Estás más delgada, no?<br />' +
+	'- Es que el negro me adelgaza...<br />' +
+	'- Pero si vas de rojo!<br />' +
+	'- Creo que no me has entendido',
+
+	'- A mi se me conquista con una buena comida.<br />' +
+	'- Te he hecho macarrones.<br />' +
+	'- Creo que no me has entendido.',
+
+	'- He denunciado al dentista por ponerme el aparato en la boca<br />' +
+	'- Si te lo ha puesto es porque lo necesitas<br />' +
+	'- Creo que no me has entendido',
+
+	'- Tu mujer me pidió que le ayudara con una estantería y acabe montándola.<br />' +
+	'- Es que no se le da nada bien el bricolaje.<br />' +
+	'- Creo que no me has entendido',
+
+	'- Necesito tu ayuda me quieren matar en Sevilla por joderles a la virgen de la Rocio.<br />' +
+	'- Normal allí son muy religiosos.<br />' +
+	'- Creo que no me has entendido',
+
+	'- Siempre que quiero follar con mi mujer dice que le duele la cabeza.<br />' +
+	'- A mi nunca me lo dice.<br />' +
+	'- Pero tú no estás casado.<br />' +
+	'- Creo que no me has entendido',
+
+	'- Mi mujer se metió ayer una piña y hemos pasado la noche en urgencias<br />' +
+	'- ¿Con el coche? ¿Pero está bien?<br />' +
+	'- Creo que no me has entendido',
+
+	'- ¡O te quitas esa falda tan corta o no sales!<br />' +
+	'- ¿Por qué, mamá?<br />' +
+	'- Porque es muy corta.<br />' +
+	'- ¿Y quéeee?<br />' +
+	'- ¡Que se te ven los huevos, José Miguel!',
+
+	'- Por el culo no, Pepe.<br />' +
+	'- Pues ya me dirás, Paco.',
+
+	'- Cariño, está lloviendo y sigues siendo una puta.<br />' +
+	'- ¿Y qué?<br />' +
+	'- Me dijiste que con el tiempo cambiarias',
+
+	'- Papá, ¿soy adoptado?<br />' +
+	'- Te elegimos negro para no tener esta conversación',
+
+	'- Cariño, ¿me engañas con otra?<br />' +
+	'- Vale.',
+
+	'- Cariño, tengo un retraso.<br />' +
+	'- Tranquila, tontita, que te quiero igual.',
+
+	'- Soy un noble caballero y sé cuando he de manchar con sangre mi espada.<br />' +
+	'- Manolo, déjate de gilipolleces, que con la regla no follo.',
+
+	'- Qué cabrón mi vecino. ¿Pues no se pone a llamar a mi puerta como un loco a las 4 de la mañana? Casi se me cae el taladro del susto.',
+
+	'- ¿Te parece bonito?<br />' +
+	'- Sí.<br />' +
+	'- Pues es rodaballo.<br />' +
+	'- Vaya.',
+
+	'- Entonces, doctor, ¿con este condón de lana se curará mi impotencia?<br />' +
+	'- No, pero le sudará la polla.',
+
+	'- Mamá, ¿cómo es que tú eres blanca, papá es negro y yo soy amarillo?<br />' +
+	'- Hijo mío, si supieras la orgía que hubo aquel día... Agradece que no ladras.',
+
+	'- Qué estrés.<br />' +
+	'- Dos más uno.',
+
+	'- Se te dan fatal las rimas.<br />' +
+	'- ¡Una mierda como una olla!',
+
+	'- ¿Sabías que las cajas negras de los aviones en realidad son naranjas?<br />' +
+	'- ¿¡No son cajas!?',
+
+	'- ¿Puedo fumar?<br />' +
+	'- Claro.<br />' +
+	'- Vale. Uno maf uno, dof. Dof maf dof, cuatro. Tref maf tref, feif.',
+
+	'- Mi marido es vidente.<br />' +
+	'- ¿Tiene dos dientes?<br />' +
+	'- ¿Tú eres tonto? ¡Que echa las cartas!<br />' +
+	'- Tú sí que eres tonta, ¡entonces será cartero!',
+
+	'- Jolín con los mosquitos.<br />' +
+	'- Ponte repelente.<br />' +
+	'- Madre mía, qué ingente cantidad de dípteros nematóceros.',
+
+	'- Cariño... ¿y si tuviéramos un hijo?<br />' +
+	'- No creo, cielo, me acordaría.',
+
+	'- ¡Papá, papá, Jaimito la tiene com un cacahuete!<br />' +
+	'- ¿De pequeña?<br />' +
+	'- No, de salada',
+
+	'Una niña sin brazos se para en una tienda de helados. El vendedor le pregunta:<br />' +
+	'- ¿De qué lo quieres, bonita?<br />' +
+	'- Pónmelo de lo que tu quieras, igualmente caerá al suelo',
+
+	'¿Cuántos negros hacen falta para lavar 1000 platos? Ninguno, es un trabajo de mujeres',
+
+	'El humor negro es como las piernas; hay quién tiene y quién no',
+
+	'¿Qué es peor que encontrarse una cucaracha en el plato? Encontrarse media',
+
+	'¿Qué no se come de un vegetal? La silla de ruedas'
+];
+
+function JokeService() {
+
+	function getTodayJoke() {
+		var today = new Date();
+		var yearlyIndex = (today.getMonth() * 31 + today.getDate());
+		return jokes[yearlyIndex % jokes.length];
+	}
+
+	return {
+		getTodayJoke
+	};
+}
+
+module.exports = JokeService();
