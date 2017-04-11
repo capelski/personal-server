3b9e21cf5574fc0d34d5180ae94d55d6af1ebb68
Carles Capellas <carlesc@byrom.plc.uk>
2017-04-11 18:01:33 +0200

    Carles: Add hamburger menu icon

--- apps/carles-capellas/public/js/binders.js
+++ apps/carles-capellas/public/js/binders.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
+	// BOXES
+
 	var initialOffsetThreshold = window.innerHeight * 0.65;
 	var offsetThreshold = window.innerHeight * 1.5;
 
@@ -16,4 +18,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
 			}
 		});
 	};
+
+	// FLIPPER
+
+	var flipper = document.getElementById('flipper');
+	var flipButton = document.getElementById('flip-button');
+
+    flipButton.onclick = function() {
+        flipper.classList.remove('block');
+        flipper.classList.toggle('front-side');
+        flipper.classList.toggle('back-side');
+        flipButton.classList.toggle('open');
+    };
 });
