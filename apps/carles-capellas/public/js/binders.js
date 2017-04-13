9b03264e559d1f5410ab979aa63bba414bdc9fda
Carles Capellas <carlesc@byrom.plc.uk>
2017-04-13 17:38:08 +0200

    Carles: Add first box content. Fix Microsoft Edge page load bug

--- apps/carles-capellas/public/js/binders.js
+++ apps/carles-capellas/public/js/binders.js
@@ -1,18 +1,18 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener('DOMContentLoaded', function(event) {
 
 	// BOXES
 
 	var initialOffsetThreshold = window.innerHeight * 0.65;
 	var offsetThreshold = window.innerHeight * 1.5;
 
-	document.querySelectorAll('.box-content').forEach(boxContent => {
+	Array.from(document.querySelectorAll('.box-content')).forEach(boxContent => {
 		if (boxContent.offsetTop > initialOffsetThreshold) {
 			boxContent.classList.add('pushed-down');
 		}
 	});
 
 	window.onscroll = function() {
-		document.querySelectorAll('.box-content').forEach(boxContent => {
+		Array.from(document.querySelectorAll('.box-content')).forEach(boxContent => {
 			if (boxContent.offsetTop <= (window.pageYOffset + offsetThreshold)) {
 				boxContent.classList.remove('pushed-down');
 			}
