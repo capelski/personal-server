172e3fad034e4fed9570a28f734a73cc28d5e9fe
Carles Capellas <carlesc@byrom.plc.uk>
2017-05-02 14:06:07 +0200

    Add timestamp on server start up message

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ var appMapper = require('./config/app-mapper');
 sassCompiler(config.hostedApps);
 appMapper(server, config.hostedApps);
 
+function zeroPrepender(value, digitsNumber) {
+	var result = value.toString();
+	while(result.length < digitsNumber) {
+		result = '0' + result;
+	}
+	return result;
+}
+
+var formatter = value => zeroPrepender(value, 2);
+
 server.listen(config.port, function () {
-	console.log('Express server listening on port ' + config.port);
+	var currentDate = new Date();
+	var formattedTime =  formatter(currentDate.getHours()) + ':' + formatter(currentDate.getMinutes()) + ':' + formatter(currentDate.getSeconds());
+	console.log(formattedTime, '-', 'Express server listening on port', config.port);
 });
