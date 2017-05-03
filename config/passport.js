c905f7a54ab129a041bf6eab7a28532e6d5d54f5
Carles Capellas <carlesc@byrom.plc.uk>
2017-05-03 17:46:34 +0200

    Modify passport in order to Isolate each app authenticated useru

--- config/passport.js
+++ config/passport.js
@@ -1,13 +1,11 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var config = require('./config');
+var strategies = {};
 
 // TODO If 404 -> Error
-
-var strategies = [];
-
-// TODO Securize access through appPath
-passport.createStrategy = function(strategyName, appPath, authenticator, deserializer) {
+// TODO Securize access through namespace
+passport.createStrategy = function(strategyName, namespace, authenticator, deserializer) {
 
 	// TODO Control strategy doesn't exist
 	// TODO Control the app name doesn't contain the APP token (||)
@@ -27,18 +25,19 @@ passport.createStrategy = function(strategyName, appPath, authenticator, deseria
 
 	passport.use(strategyName, localStrategy);
 
-	strategies.push({
-		name: strategyName,
-		deserializer: deserializer
-	});
+	strategies[namespace] = {
+		deserializer: deserializer,
+		currentUser: null
+	};
 
 	return function(req, res, next) {
 		function userAuthenticated(error, user, info) {
 	        if (error) return res.status(401).json(error);
 
-			user._strategyName = strategyName;
 	        req.logIn(user, function(error) {
 	            if (error) return res.send(error);
+
+	            strategies[namespace].currentUser = user.id;
 	            res.json(req.user);
 	        });
 	    }
@@ -48,17 +47,16 @@ passport.createStrategy = function(strategyName, appPath, authenticator, deseria
 }
 
 passport.serializeUser(function (user, done) {
-	var strategyName = user._strategyName;
-	delete user._strategyName;
-    return done(null, strategyName + '||' + user.id);
+    return done(null, user.id);
 });
 
 // TODO Remove the strategyName approach; the token is overriden, so we can't rely on it
 passport.deserializeUser(function (userToken, userAuthenticated) {
+console.log(userToken);
 	var decomposition = userToken.split('||');
-	var strategyName = decomposition[0];
+	var strategyNamespace = decomposition[0];
 	var userId = decomposition[1];
-	var strategy = strategies.find(strategy => strategy.name === strategyName);
+	var strategy = strategies[strategyNamespace];
 
 	//TODO Instead of the strategyName, use the application path to get the strategy
 
@@ -78,12 +76,20 @@ module.exports = function (server) {
 	var authenticatedApps = config.hostedApps.filter(app => app.enableAuthentication);
 	server.use(function(req, res, next) {
 		// TODO Exclude static resources
-
 		var matched = false;
 		authenticatedApps.forEach(app => {
 			if (req.url.startsWith('/' + app.namespace)) {
 				matched = true;
 				// TODO Manipulate the request so the deserialize will work
+				var tenancy = strategies[app.namespace];
+				if (req.session.passport) {
+					if(tenancy.currentUser) {
+						req.session.passport.user = app.namespace + '||' + tenancy.currentUser;
+					}
+					else {
+						delete req.session.passport;
+					}
+				} 
 				passport.session()(req, res, next)
 			}
 		});
