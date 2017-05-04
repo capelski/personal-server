6d8b97668f3ddac73155d7525752d7f150eec710
Carles Capellas <carlesc@byrom.plc.uk>
2017-05-04 13:22:49 +0200

    Skip authentication for static resources urls. Refactor passport code

--- config/passport.js
+++ config/passport.js
@@ -1,44 +1,93 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var config = require('./config');
-var strategies = {};
-var userTokenSeparator = '||->';
 
-// TODO If 404 -> Error
+var authenticatedApps = config.hostedApps.filter(app => app.enableAuthentication);
+var deserializers = {};
+var userPrefixSeparator = '||->';
+
+function getUserFromPrefixedUserId(prefixedUserId) {
+	var decomposition = prefixedUserId.split(userPrefixSeparator);
+	var namespace = decomposition[0];
+	var userId = decomposition[1];
+    return deserializers[namespace](userId);
+}
+
+function isStaticContent(relativeUrl) {
+	return relativeUrl.indexOf('/js/') > -1 || relativeUrl.indexOf('/css/') > -1 ;
+}
+
+function prefixUserId(currentApp, userSession) {
+	if (userSession.passport && userSession.authDomains[currentApp.namespace]) {
+		userSession.passport.user = currentApp.namespace + userPrefixSeparator + userSession.authDomains[currentApp.namespace];
+	}
+	else {
+		delete userSession.passport;
+	}
+}
+
+function userRetrievedHandler(userAuthenticated) {
+	return function (user) {
+    	if (!user) return userAuthenticated({
+    		message: 'Incorrect username or password'
+    	}, null);
+		// console.log('Retrieved user:', user);
+    	return userAuthenticated(null, user);
+    };
+}
+
+module.exports = function (server) {
+	server.use(passport.initialize());
+	server.use(function (req, res, next) {
+
+		if (isStaticContent(req.url)) return next();
+
+		var currentApp = authenticatedApps.find(app => req.url.startsWith('/' + app.namespace));
+		if (!currentApp) return next();
+
+		prefixUserId(currentApp, req.session);
+		return passport.session()(req, res, next);
+	});
+
+	passport.serializeUser(function (user, done) {
+    	return done(null, user.id);
+	});
+
+	passport.deserializeUser(function (prefixedUserId, userAuthenticated) {
+	    return getUserFromPrefixedUserId(prefixedUserId)
+	    .then(userRetrievedHandler(userAuthenticated));
+	});
+};
+
 // TODO Securize access through namespace
 
-passport.createStrategy = function(namespace, authenticator, deserializer) {
+passport.createStrategy = function (namespace, authenticator, deserializer) {
 
-	if (namespace.indexOf(userTokenSeparator) > -1) {
+	if (namespace.indexOf(userPrefixSeparator) > -1) {
 		throw 'The namespace ' + namespace + ' is not valid!';
 	}
 
-	strategies[namespace] = {
-		deserializer: deserializer
-	};
+	deserializers[namespace] = deserializer;
 
 	var localStrategy = new LocalStrategy({
 	    usernameField: 'username',
 	    passwordField: 'password'
 	}, function (username, password, userAuthenticated) {
 	    return authenticator(username, password)
-	    .then(user => {
-	    	if (!user) return userAuthenticated({
-	    		message: 'Incorrect username or password'
-	    	}, null);
-	    	return userAuthenticated(null, user);
-	    });
+	    .then(userRetrievedHandler(userAuthenticated));
 	});
 
 	passport.use(namespace, localStrategy);
 
-	return function(req, res, next) {
+	return function (req, res, next) {
+
 		function userAuthenticated(error, user, info) {
 	        if (error) return res.status(401).json(error);
 
-	        req.logIn(user, function(error) {
+	        req.logIn(user, function (error) {
 	            if (error) return res.send(error);
 
+				req.session.authDomains = req.session.authDomains || {};
 	            req.session.authDomains[namespace] = user.id;
 	            res.json(req.user);
 	        });
@@ -47,53 +96,3 @@ passport.createStrategy = function(namespace, authenticator, deserializer) {
 	    passport.authenticate(namespace, userAuthenticated)(req, res, next);
 	};
 }
-
-passport.serializeUser(function (user, done) {
-    return done(null, user.id);
-});
-
-passport.deserializeUser(function (userToken, userAuthenticated) {
-	var decomposition = userToken.split(userTokenSeparator);
-	var strategyNamespace = decomposition[0];
-	var userId = decomposition[1];
-	var strategy = strategies[strategyNamespace];
-
-    return strategy.deserializer(userId)
-    .then(user => {
-    	if (!user) return userAuthenticated({
-    		message: 'Incorrect username or password'
-    	}, null);
-    	return userAuthenticated(null, user);
-    });
-});
-
-module.exports = function (server) {
-
-	server.use(passport.initialize());
-
-	var authenticatedApps = config.hostedApps.filter(app => app.enableAuthentication);
-	server.use(function(req, res, next) {
-		// TODO Exclude static resources
-		var matched = false;
-		authenticatedApps.forEach(app => {
-			if (req.url.startsWith('/' + app.namespace)) {
-				matched = true;
-
-				req.session.authDomains = req.session.authDomains || {};
-				if (req.session.passport) {
-					if(req.session.authDomains[app.namespace]) {
-						req.session.passport.user = app.namespace + userTokenSeparator + req.session.authDomains[app.namespace];
-					}
-					else {
-						delete req.session.passport;
-					}
-				} 
-				passport.session()(req, res, next)
-			}
-		});
-		
-		if (!matched) {
-			next();
-		}
-	});
-};
