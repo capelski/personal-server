e809710e1fef00f76d7f71935d717c71748fa372
Carles Capellas <carlesc@byrom.plc.uk>
2017-05-11 12:56:17 +0200

    Users template: Move generic security methods to security.js util

--- apps/users-template/controllers/elements-security.js
+++ apps/users-template/controllers/elements-security.js
@@ -1,38 +1,16 @@
 var security = require('../../../utils/security');
 
 function elementAccess (req, res, accessType, element) {
-	var allowedAccess = elementAccessCriteria(element, req.user);
+	var allowedAccess = !element || (
+		(element.public || (req.user && req.user.id === element.ownerId)) &&
+		(!element.restricted || (req.user && security.hasUserPermission(req.user, 'view-restricted')))
+	);
 	if (accessType === 'view') {
-		return securizeView(res, allowedAccess, 'users-template-unauthorized');
+		return security.securizeView(res, allowedAccess, 'users-template-unauthorized');
 	}
 	else if (accessType == 'api') {
-		return securizeApi(res, allowedAccess, 'You are not allowed to view the element');
+		return security.securizeApi(res, allowedAccess, 'You are not allowed to view the element');
 	}
-}
-
-function elementAccessCriteria(element, user) {
-	return !element || (
-		(element.public || (user && user.id === element.ownerId)) &&
-		(!element.restricted || (user && security.hasUserPermission(user, 'view-restricted')))
-	);
-}
-
-function securizeView (res, allowedAccess, unauthorizedView) {
-	return new Promise(function(resolve, reject) {
-		if (!allowedAccess) {
-			return res.render(unauthorizedView);
-		}
-		return resolve();
-	});
-}
-
-function securizeApi (res, allowedAccess, unauthorizedMessage) {
-	return new Promise(function(resolve, reject) {
-		if (!allowedAccess) {
-			return res.json(unauthorizedMessage);
-		}
-		return resolve();
-	});
 }
 
 module.exports = {
--- utils/security.js
+++ utils/security.js
@@ -8,9 +8,29 @@ function security() {
 		return user && user.permissions.indexOf(permission) > -1;
 	}
 
+	function securizeApi (res, allowedAccess, unauthorizedMessage) {
+		return new Promise(function(resolve, reject) {
+			if (!allowedAccess) {
+				return res.status(401).json(unauthorizedMessage);
+			}
+			return resolve();
+		});
+	}
+
+	function securizeView (res, allowedAccess, unauthorizedView) {
+		return new Promise(function(resolve, reject) {
+			if (!allowedAccess) {
+				return res.status(401).render(unauthorizedView);
+			}
+			return resolve();
+		});
+	}
+
 	return {
 		hasUserPermission,
-		isUserAuthenticated
+		isUserAuthenticated,
+		securizeApi,
+		securizeView
 	};
 }
 
