4b8ad44ca3a90e52b9ba06ba593d95f90561ec22
Carles Capellas <capellas.carles@gmail.com>
2018-03-11 22:20:55 +0100

    dragon-pong: Remove app name from views files

--- apps/dragon-pong/index.js
+++ apps/dragon-pong/index.js
@@ -5,7 +5,7 @@ var matchesController = require('./controllers/matches-controller');
 
 const configureRouter = (middleware) => {
 	router.get('/', function (req, res, next) {
-	  res.render('dragon-pong');
+	  res.render('index');
 	});
 	router.get('/matches', matchesController.getAll);
 	return router;
