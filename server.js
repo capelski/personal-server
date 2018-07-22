99c8fff753edea4db8f70a132fd5987fd2d1dc8d
Carles Capellas Careta <capellas.carles@gmail.com>
2018-07-22 20:06:31 +0200

    Remove server port configuration from config files

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const { join } = require('path');
 const { runServer } = require('modena');
 const config = require('./config/config');
 
+config.PORT = 80;
+
 config.beforeRegisterApps = (server, modenaConfig, appsConfig, express) => {
 	const pluginsPath = join(__dirname, 'plugins');
 	server.use('/plugins', express.static(pluginsPath));
