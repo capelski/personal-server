39979c004a580135a05bbd5123cc725e2b99b543
Carles Capellas Careta <capellas.carles@gmail.com>
2018-07-29 21:56:16 +0200

    Update modena version. Update skills-matrix-api-node submodule

--- config/index.js
+++ config/index.js
@@ -1,6 +1,8 @@
 const { join } = require('path');
 const { existsSync } = require('fs');
 
+// TODO Extract this check to modena too; allow providing a path (string) instead of options
+
 let config = {};
 if (existsSync(join(__dirname, 'config.json'))) {
     config = require('./config.json');
